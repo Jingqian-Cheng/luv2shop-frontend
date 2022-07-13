3c24c2d547f3004b303d6a5523cc43b0399f21ab
Jingqian Cheng <jingqiancheng@brandeis.edu>
2022-07-13 14:41:17 -0400

    final version

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa5pps66tzTEhRG05d7',
+        issuer: 'https://dev-90503354.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+}
---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,38 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { from, lastValueFrom, Observable } from 'rxjs';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor{
+
+  constructor(private oktaAuthService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handlleAccess(request, next));
+  }
+
+  private async handlleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    
+    // Only add an access token for secured endpoints
+    const theEndpoint = environment.luv2shopApiUrl + '/orders';
+    const securedEndpoints = [theEndpoint];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      const accessToken = await this.oktaAuth.getAccessToken();
+    
+      // clone the request and add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+    return await lastValueFrom(next.handle(request));
+  }
+}
